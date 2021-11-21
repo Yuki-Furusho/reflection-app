47371e3c966d3de60afb918f3adc211e33283e69
Yuki-Furusho <furusho@sikmi.com>
2021-11-21 21:27:37 +0900

    deviseの導入

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,6 +1,7 @@
 class Note < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :visibility
+  belongs_to :user
 
   validates :fact, :visibility_id, presence: true
 end
