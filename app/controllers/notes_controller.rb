b8761f32a5413ddbca32f7381fa8e49f95ece582
Yuki-Furusho <furusho@sikmi.com>
2021-11-23 20:56:14 +0900

    notes/newページの作成

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -4,5 +4,22 @@ class NotesController < ApplicationController
   end
 
   def new
+    @note = Note.new
+  end
+
+  def create
+    @note = Note.new(note_params)
+    if @note.save
+      session[:note_id] = @note.id
+      redirect_to note_steps_path(:emotion)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def note_params
+    params.require(:note).permit(:when, :fact, :emotions, :visibility_id, :wanted_to, :wanted_you_to).merge(user_id: current_user.id)
   end
 end
--- app/models/note.rb
+++ app/models/note.rb
@@ -2,6 +2,7 @@ class Note < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :visibility
   belongs_to :user
+  has_many :emotions
 
-  validates :fact, :visibility_id, presence: true
+  validates :fact, presence: true
 end
