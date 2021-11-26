e28a8d467c224401a67549b325d77e1b365ac427
Yuki-Furusho <furusho@sikmi.com>
2021-11-27 00:56:22 +0900

    編集画面・削除処理の実装

--- app/models/note.rb
+++ app/models/note.rb
@@ -2,8 +2,8 @@ class Note < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   has_one :visibility
   belongs_to :user
-  has_many :note_emotions
-  has_many :emotions, through: :note_emotions
+  has_many :note_emotions, dependent: :destroy
+  has_many :emotions, through: :note_emotions, dependent: :destroy
 
   validates :fact, presence: true
 end
